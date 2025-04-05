6170e636e6ea8a2a47ef2a113a8b01b64884c89e
Benjamin <goosta19802@gmail.com>
2025-04-05 20:56:50 -0300

    Peque;o toque

--- src/components/Forms/FormAltaNovedad.jsx
+++ src/components/Forms/FormAltaNovedad.jsx
@@ -14,25 +14,25 @@
  * Contacto: benjamin.orellanaof@gmail.com || 3863531891
  */
 
-import React, { useState, useEffect, useRef } from "react"; // (NUEVO)
-import axios from "axios";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-import ModalSuccess from "./ModalSuccess";
-import ModalError from "./ModalError";
-import Alerta from "../Error";
+import React, { useState, useEffect, useRef } from 'react'; // (NUEVO)
+import axios from 'axios';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import ModalSuccess from './ModalSuccess';
+import ModalError from './ModalError';
+import Alerta from '../Error';
 
 const FormAltaNovedad = ({
   isOpen,
   onClose,
   novedad,
   setSelectedNovedad,
-  obtenerNovedades,
+  obtenerNovedades
 }) => {
   const [users, setUsers] = useState([]);
-  const [selectedSede, setSelectedSede] = useState(["monteros"]);
+  const [selectedSede, setSelectedSede] = useState(['monteros']);
   const [selectedUsers, setSelectedUsers] = useState([]);
   const [selectAllUsers, setSelectAllUsers] = useState(false);
 
@@ -41,16 +41,16 @@ const FormAltaNovedad = ({
 
   // const textoModal = 'Usuario creado correctamente.'; se elimina el texto
   // nuevo estado para gestionar dinámicamente según el método (PUT o POST)
-  const [textoModal, setTextoModal] = useState("");
+  const [textoModal, setTextoModal] = useState('');
 
   // nueva variable para administrar el contenido de formulario para saber cuando limpiarlo
   const formikRef = useRef(null);
 
   const nuevoNovedadSchema = Yup.object().shape({
-    titulo: Yup.string().required("El Titulo es obligatorio"),
+    titulo: Yup.string().required('El Titulo es obligatorio'),
     /* sede: Yup.string().required("La Sede es obligatoria"), */
-    mensaje: Yup.string().required("El Mensaje es obligatorio"),
-    vencimiento: Yup.date().nullable(true),
+    mensaje: Yup.string().required('El Mensaje es obligatorio'),
+    vencimiento: Yup.date().nullable(true)
   });
 
   useEffect(() => {
@@ -67,20 +67,20 @@ const FormAltaNovedad = ({
   const obtenerUsers = async (sede) => {
     try {
       const response =
-        sede === "todas" || sede === ""
-          ? await axios.get("http://localhost:8080/users")
-          : await axios.get("http://localhost:8080/users", {
-              params: { sede },
+        sede === 'todas' || sede === ''
+          ? await axios.get('http://localhost:8080/users')
+          : await axios.get('http://localhost:8080/users', {
+              params: { sede }
             });
 
       // Filtrar los usuarios para excluir aquellos con level = 'instructor'
       const usuariosFiltrados = response.data.filter(
-        (user) => user.level !== "instructor"
+        (user) => user.level !== 'instructor'
       );
 
       setUsers(usuariosFiltrados);
     } catch (error) {
-      console.log("Error al obtener los usuarios:", error);
+      console.log('Error al obtener los usuarios:', error);
       setUsers([]);
     }
   };
@@ -106,9 +106,9 @@ const FormAltaNovedad = ({
   };
 
   const formatSedeValue = (selectedSede) => {
-    return selectedSede.length === 3 ? "todas" : selectedSede.join(", ");
+    return selectedSede.length === 3 ? 'todas' : selectedSede.join(', ');
   };
-  
+
   const handleSubmitNovedad = async (valores) => {
     try {
       valores.sede = formatSedeValue(selectedSede);
@@ -118,43 +118,43 @@ const FormAltaNovedad = ({
         mensaje: valores.mensaje,
         vencimiento: valores.vencimiento,
         estado: valores.estado,
-        user: selectedUsers,
+        user: selectedUsers
       };
       // Definir URL y método basados en la existencia de novedad
       const url = novedad
         ? `http://localhost:8080/novedades/${novedad.id}`
-        : "http://localhost:8080/novedades/";
-      const method = novedad ? "PUT" : "POST";
+        : 'http://localhost:8080/novedades/';
+      const method = novedad ? 'PUT' : 'POST';
 
       const respuesta = await fetch(url, {
         method: method,
         body: JSON.stringify(data),
         headers: {
-          "Content-Type": "application/json",
-        },
+          'Content-Type': 'application/json'
+        }
       });
 
-      if (method === "PUT") {
+      if (method === 'PUT') {
         // setName(null); // una vez que sale del metodo PUT, limpiamos el campo descripcion
-        setTextoModal("Novedad actualizada correctamente.");
+        setTextoModal('Novedad actualizada correctamente.');
       } else {
-        setTextoModal("Novedad creada correctamente.");
+        setTextoModal('Novedad creada correctamente.');
       }
 
       // Verificamos si la solicitud fue exitosa
       if (!respuesta.ok) {
-        throw new Error("Error en la solicitud ${method}: " + respuesta.status);
+        throw new Error('Error en la solicitud ${method}: ' + respuesta.status);
       }
 
       obtenerNovedades();
 
       const result = await respuesta.json();
-      console.log("Registro insertado correctamente:", result);
+      console.log('Registro insertado correctamente:', result);
 
       setShowModal(true);
       setTimeout(() => setShowModal(false), 1500);
     } catch (error) {
-      console.error("Error al insertar el registro:", error.message);
+      console.error('Error al insertar el registro:', error.message);
       setErrorModal(true);
       setTimeout(() => setErrorModal(false), 1500);
     }
@@ -180,19 +180,18 @@ const FormAltaNovedad = ({
   return (
     <div
       className={`h-screen w-screen p-2 sm:px-0 mt-16 fixed inset-0 flex pt-10 justify-center ${
-        isOpen ? "block" : "hidden"
+        isOpen ? 'block' : 'hidden'
       } bg-gray-800 bg-opacity-75 z-50`}
     >
       <div className="container-inputs">
         <Formik
           innerRef={formikRef}
-          
           initialValues={{
             sede: selectedSede,
-            titulo: novedad ? novedad.titulo : "",
-            mensaje: novedad ? novedad.mensaje : "",
-            vencimiento: novedad ? novedad.vencimiento : "",
-            estado: 1,
+            titulo: novedad ? novedad.titulo : '',
+            mensaje: novedad ? novedad.mensaje : '',
+            vencimiento: novedad ? novedad.vencimiento : '',
+            estado: 1
           }}
           enableReinitialize
           onSubmit={async (values, { resetForm }) => {
@@ -228,41 +227,41 @@ const FormAltaNovedad = ({
                   <button
                     type="button"
                     className={`w-full py-2 px-5 rounded-xl text-white text-sm font-bold transition ${
-                      selectedSede.includes("monteros")
-                        ? "bg-[#fc4b08]"
-                        : "bg-orange-500 hover:bg-[#fc4b08]"
+                      selectedSede.includes('monteros')
+                        ? 'bg-[#fc4b08]'
+                        : 'bg-orange-500 hover:bg-[#fc4b08]'
                     } focus:outline-orange-100`}
-                    onClick={() => handleSedeSelection("monteros")}
+                    onClick={() => handleSedeSelection('monteros')}
                   >
-                    {selectedSede.includes("monteros")
-                      ? "Monteros✅"
-                      : "Monteros"}
+                    {selectedSede.includes('monteros')
+                      ? 'Monteros✅'
+                      : 'Monteros'}
                   </button>
 
                   <button
                     type="button"
                     className={`w-full py-2 px-5 rounded-xl text-white text-sm font-bold transition ${
-                      selectedSede.includes("concepcion")
-                        ? "bg-[#fc4b08]"
-                        : "bg-orange-500 hover:bg-[#fc4b08]"
+                      selectedSede.includes('concepcion')
+                        ? 'bg-[#fc4b08]'
+                        : 'bg-orange-500 hover:bg-[#fc4b08]'
                     } focus:outline-orange-100`}
-                    onClick={() => handleSedeSelection("concepcion")}
+                    onClick={() => handleSedeSelection('concepcion')}
                   >
-                    {selectedSede.includes("concepcion")
-                      ? "Concepción✅"
-                      : "Concepción"}
+                    {selectedSede.includes('concepcion')
+                      ? 'Concepción✅'
+                      : 'Concepción'}
                   </button>
 
                   <button
                     type="button"
                     className={`w-full py-2 px-5 rounded-xl text-white text-sm font-bold transition ${
-                      selectedSede.includes("SMT")
-                        ? "bg-[#fc4b08]"
-                        : "bg-orange-500 hover:bg-[#fc4b08]"
+                      selectedSede.includes('SMT')
+                        ? 'bg-[#fc4b08]'
+                        : 'bg-orange-500 hover:bg-[#fc4b08]'
                     } focus:outline-orange-100`}
-                    onClick={() => handleSedeSelection("SMT")}
+                    onClick={() => handleSedeSelection('SMT')}
                   >
-                    {selectedSede.includes("SMT") ? "SMT✅" : "SMT"}
+                    {selectedSede.includes('SMT') ? 'SMT✅' : 'SMT'}
                   </button>
                 </div>
 
@@ -282,7 +281,10 @@ const FormAltaNovedad = ({
                             htmlFor="select-all-users"
                             className="ml-2 text-sm font-medium text-gray-700 cursor-pointer"
                           >
-                            Seleccionar todos los usuarios
+                            Seleccionar todos los usuarios de{' '}
+                            <p className="font-bold uppercase">
+                              {formatSedeValue(selectedSede)}
+                            </p>
                           </label>
                         </div>
                         <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
@@ -302,7 +304,7 @@ const FormAltaNovedad = ({
                               <label
                                 htmlFor={`user-${user.id}`}
                                 className="ml-3 text- text-gray-800 cursor-pointer truncate"
-                                style={{ fontSize: "0.775rem" }}
+                                style={{ fontSize: '0.775rem' }}
                               >
                                 {user.name}
                               </label>
@@ -352,7 +354,7 @@ const FormAltaNovedad = ({
                   <ReactQuill
                     theme="snow"
                     value={values.mensaje}
-                    onChange={(content) => setFieldValue("mensaje", content)}
+                    onChange={(content) => setFieldValue('mensaje', content)}
                     placeholder="Ingrese el mensaje"
                     className="mt-2 block w-full p-3 text-black bg-slate-100 rounded-xl focus:outline-orange-500"
                   />
@@ -364,7 +366,7 @@ const FormAltaNovedad = ({
                 <div className="sticky bottom-0 bg-white py-3 px-4">
                   <input
                     type="submit"
-                    value={novedad ? "Actualizar" : "Crear Novedad"}
+                    value={novedad ? 'Actualizar' : 'Crear Novedad'}
                     className="w-full bg-orange-500 py-2 px-5 rounded-xl text-white font-bold hover:bg-[#fc4b08] focus:outline-orange-100"
                   />
                 </div>
