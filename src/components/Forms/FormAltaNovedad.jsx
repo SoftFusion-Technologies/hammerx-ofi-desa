adfd1e315981866554438fda61dea2f9921d3fe8
Sergio Manrique <sergiomanrique@live.com>
2025-04-05 20:11:30 -0300

    Rediseño de la interfaz y funcionalidad de FormAltaNovedad

--- src/components/Forms/FormAltaNovedad.jsx
+++ src/components/Forms/FormAltaNovedad.jsx
@@ -14,25 +14,25 @@
  * Contacto: benjamin.orellanaof@gmail.com || 3863531891
  */
 
-import React, { useState, useEffect, useRef } from 'react'; // (NUEVO)
-import axios from 'axios';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import ModalSuccess from './ModalSuccess';
-import ModalError from './ModalError';
-import Alerta from '../Error';
+import React, { useState, useEffect, useRef } from "react"; // (NUEVO)
+import axios from "axios";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import ModalSuccess from "./ModalSuccess";
+import ModalError from "./ModalError";
+import Alerta from "../Error";
 
 const FormAltaNovedad = ({
   isOpen,
   onClose,
   novedad,
   setSelectedNovedad,
-  obtenerNovedades
+  obtenerNovedades,
 }) => {
   const [users, setUsers] = useState([]);
-  const [selectedSede, setSelectedSede] = useState('');
+  const [selectedSede, setSelectedSede] = useState(["monteros"]);
   const [selectedUsers, setSelectedUsers] = useState([]);
   const [selectAllUsers, setSelectAllUsers] = useState(false);
 
@@ -41,16 +41,16 @@ const FormAltaNovedad = ({
 
   // const textoModal = 'Usuario creado correctamente.'; se elimina el texto
   // nuevo estado para gestionar dinámicamente según el método (PUT o POST)
-  const [textoModal, setTextoModal] = useState('');
+  const [textoModal, setTextoModal] = useState("");
 
   // nueva variable para administrar el contenido de formulario para saber cuando limpiarlo
   const formikRef = useRef(null);
 
   const nuevoNovedadSchema = Yup.object().shape({
-    titulo: Yup.string().required('El Titulo es obligatorio'),
-    sede: Yup.string().required('La Sede es obligatoria'),
-    mensaje: Yup.string().required('El Mensaje es obligatorio'),
-    vencimiento: Yup.date().nullable(true)
+    titulo: Yup.string().required("El Titulo es obligatorio"),
+    /* sede: Yup.string().required("La Sede es obligatoria"), */
+    mensaje: Yup.string().required("El Mensaje es obligatorio"),
+    vencimiento: Yup.date().nullable(true),
   });
 
   useEffect(() => {
@@ -59,23 +59,28 @@ const FormAltaNovedad = ({
     }
   }, [isOpen, selectedSede]);
 
+  useEffect(() => {
+    setSelectedUsers([]);
+    setSelectAllUsers(false);
+  }, [selectedSede]);
+
   const obtenerUsers = async (sede) => {
     try {
       const response =
-        sede === 'todas' || sede === ''
-          ? await axios.get('http://localhost:8080/users')
-          : await axios.get('http://localhost:8080/users', {
-              params: { sede }
+        sede === "todas" || sede === ""
+          ? await axios.get("http://localhost:8080/users")
+          : await axios.get("http://localhost:8080/users", {
+              params: { sede },
             });
 
       // Filtrar los usuarios para excluir aquellos con level = 'instructor'
       const usuariosFiltrados = response.data.filter(
-        (user) => user.level !== 'instructor'
+        (user) => user.level !== "instructor"
       );
 
       setUsers(usuariosFiltrados);
     } catch (error) {
-      console.log('Error al obtener los usuarios:', error);
+      console.log("Error al obtener los usuarios:", error);
       setUsers([]);
     }
   };
@@ -99,56 +104,62 @@ const FormAltaNovedad = ({
       setSelectedUsers([]);
     }
   };
+
+  const formatSedeValue = (selectedSede) => {
+    return selectedSede.length === 3 ? "todas" : selectedSede.join(", ");
+  };
+  
   const handleSubmitNovedad = async (valores) => {
     try {
+      valores.sede = formatSedeValue(selectedSede);
       const data = {
         sede: valores.sede,
         titulo: valores.titulo,
         mensaje: valores.mensaje,
         vencimiento: valores.vencimiento,
         estado: valores.estado,
-        user: selectedUsers
+        user: selectedUsers,
       };
-
       // Definir URL y método basados en la existencia de novedad
       const url = novedad
         ? `http://localhost:8080/novedades/${novedad.id}`
-        : 'http://localhost:8080/novedades/';
-      const method = novedad ? 'PUT' : 'POST';
+        : "http://localhost:8080/novedades/";
+      const method = novedad ? "PUT" : "POST";
 
       const respuesta = await fetch(url, {
         method: method,
         body: JSON.stringify(data),
         headers: {
-          'Content-Type': 'application/json'
-        }
+          "Content-Type": "application/json",
+        },
       });
 
-      if (method === 'PUT') {
+      if (method === "PUT") {
         // setName(null); // una vez que sale del metodo PUT, limpiamos el campo descripcion
-        setTextoModal('Novedad actualizada correctamente.');
+        setTextoModal("Novedad actualizada correctamente.");
       } else {
-        setTextoModal('Novedad creada correctamente.');
+        setTextoModal("Novedad creada correctamente.");
       }
 
       // Verificamos si la solicitud fue exitosa
       if (!respuesta.ok) {
-        throw new Error('Error en la solicitud ${method}: ' + respuesta.status);
+        throw new Error("Error en la solicitud ${method}: " + respuesta.status);
       }
 
       obtenerNovedades();
 
       const result = await respuesta.json();
-      console.log('Registro insertado correctamente:', result);
+      console.log("Registro insertado correctamente:", result);
 
       setShowModal(true);
       setTimeout(() => setShowModal(false), 1500);
     } catch (error) {
-      console.error('Error al insertar el registro:', error.message);
+      console.error("Error al insertar el registro:", error.message);
       setErrorModal(true);
       setTimeout(() => setErrorModal(false), 1500);
     }
   };
+
   const handleClose = () => {
     if (formikRef.current) {
       formikRef.current.resetForm();
@@ -157,21 +168,31 @@ const FormAltaNovedad = ({
     onClose();
   };
 
+  const handleSedeSelection = (sede) => {
+    if (selectedSede.length === 1 && selectedSede.includes(sede)) return;
+    setSelectedSede((prev) =>
+      prev.includes(sede)
+        ? prev.filter((item) => item !== sede)
+        : [...prev, sede]
+    );
+  };
+
   return (
     <div
-      className={`h-screen w-screen mt-16 fixed inset-0 flex pt-10 justify-center ${
-        isOpen ? 'block' : 'hidden'
+      className={`h-screen w-screen p-2 sm:px-0 mt-16 fixed inset-0 flex pt-10 justify-center ${
+        isOpen ? "block" : "hidden"
       } bg-gray-800 bg-opacity-75 z-50`}
     >
       <div className="container-inputs">
         <Formik
           innerRef={formikRef}
+          
           initialValues={{
-            sede: novedad ? novedad.sede : '',
-            titulo: novedad ? novedad.titulo : '',
-            mensaje: novedad ? novedad.mensaje : '',
-            vencimiento: novedad ? novedad.vencimiento : '',
-            estado: 1
+            sede: selectedSede,
+            titulo: novedad ? novedad.titulo : "",
+            mensaje: novedad ? novedad.mensaje : "",
+            vencimiento: novedad ? novedad.vencimiento : "",
+            estado: 1,
           }}
           enableReinitialize
           onSubmit={async (values, { resetForm }) => {
@@ -181,7 +202,7 @@ const FormAltaNovedad = ({
           validationSchema={nuevoNovedadSchema}
         >
           {({ errors, touched, setFieldValue, values }) => (
-            <div className="-mt-20 max-h-screen w-full max-w-md overflow-y-auto bg-white rounded-xl p-5">
+            <div className="-mt-20 max-h-screen w-full max-w-xl overflow-y-auto bg-white rounded-xl p-5">
               <Form className="formulario w-full bg-white">
                 <div className="flex justify-between">
                   <div className="tools">
@@ -203,74 +224,92 @@ const FormAltaNovedad = ({
                   </div>
                 </div>
 
-                <div className="mb-4 px-4">
-                  <Field
-                    as="select"
-                    id="sede"
-                    name="sede"
-                    className="form-select mt-2 block w-full p-3 text-black bg-slate-100 rounded-xl focus:outline-orange-500"
-                    onChange={(e) => {
-                      const selected = e.target.value;
-                      setFieldValue('sede', selected);
-                      setSelectedSede(selected);
-                    }}
-                    required
+                <div className="grid grid-cols-1 sm:grid-cols-3 gap-3 mx-2">
+                  <button
+                    type="button"
+                    className={`w-full py-2 px-5 rounded-xl text-white text-sm font-bold transition ${
+                      selectedSede.includes("monteros")
+                        ? "bg-[#fc4b08]"
+                        : "bg-orange-500 hover:bg-[#fc4b08]"
+                    } focus:outline-orange-100`}
+                    onClick={() => handleSedeSelection("monteros")}
                   >
-                    <option value="" disabled>
-                      Selecciona tu sucursal
-                    </option>
-                    <option value="todas">Sucursal: Todas</option>
-                    <option value="SMT">SMT</option>
-                    <option value="monteros">Monteros</option>
-                    <option value="concepcion">Concepción</option>
-                  </Field>
-                  {errors.sede && touched.sede ? (
-                    <Alerta>{errors.sede}</Alerta>
-                  ) : null}
+                    {selectedSede.includes("monteros")
+                      ? "Monteros✅"
+                      : "Monteros"}
+                  </button>
+
+                  <button
+                    type="button"
+                    className={`w-full py-2 px-5 rounded-xl text-white text-sm font-bold transition ${
+                      selectedSede.includes("concepcion")
+                        ? "bg-[#fc4b08]"
+                        : "bg-orange-500 hover:bg-[#fc4b08]"
+                    } focus:outline-orange-100`}
+                    onClick={() => handleSedeSelection("concepcion")}
+                  >
+                    {selectedSede.includes("concepcion")
+                      ? "Concepción✅"
+                      : "Concepción"}
+                  </button>
+
+                  <button
+                    type="button"
+                    className={`w-full py-2 px-5 rounded-xl text-white text-sm font-bold transition ${
+                      selectedSede.includes("SMT")
+                        ? "bg-[#fc4b08]"
+                        : "bg-orange-500 hover:bg-[#fc4b08]"
+                    } focus:outline-orange-100`}
+                    onClick={() => handleSedeSelection("SMT")}
+                  >
+                    {selectedSede.includes("SMT") ? "SMT✅" : "SMT"}
+                  </button>
                 </div>
 
                 <div className="mb-6 px-6 py-4 bg-white rounded-lg shadow-md">
-                  <div className="mb-4 flex items-center">
-                    <input
-                      type="checkbox"
-                      id="select-all-users"
-                      className="form-checkbox h-5 w-5 text-blue-600 rounded focus:ring-blue-500"
-                      onChange={handleSelectAllUsers}
-                      checked={selectAllUsers}
-                    />
-                    <label
-                      htmlFor="select-all-users"
-                      className="ml-2 text-sm font-medium text-gray-700 cursor-pointer"
-                    >
-                      Seleccionar todos los usuarios
-                    </label>
-                  </div>
-
                   <div className="max-h-64 overflow-y-auto scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-gray-200">
                     {Array.isArray(users) && users.length > 0 ? (
-                      <div className="space-y-3">
-                        {users.map((user) => (
-                          <div
-                            key={user.id}
-                            className="flex items-center rounded-lg border border-gray-200 px-4 py-2 hover:bg-gray-100"
+                      <>
+                        <div className="mb-4 flex items-center">
+                          <input
+                            type="checkbox"
+                            id="select-all-users"
+                            className="form-checkbox h-5 w-5 text-blue-600 rounded focus:ring-blue-500"
+                            onChange={handleSelectAllUsers}
+                            checked={selectAllUsers}
+                          />
+                          <label
+                            htmlFor="select-all-users"
+                            className="ml-2 text-sm font-medium text-gray-700 cursor-pointer"
                           >
-                            <input
-                              type="checkbox"
-                              id={`user-${user.id}`}
-                              className="form-checkbox h-5 w-5 text-blue-600 rounded focus:ring-blue-500"
-                              value={user.id}
-                              onChange={() => handleCheckboxChange(user.id)}
-                              checked={selectedUsers.includes(user.id)}
-                            />
-                            <label
-                              htmlFor={`user-${user.id}`}
-                              className="ml-3 text-sm font-medium text-gray-800 cursor-pointer"
+                            Seleccionar todos los usuarios
+                          </label>
+                        </div>
+                        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                          {users.map((user) => (
+                            <div
+                              key={user.id}
+                              className="flex items-center rounded-lg border border-gray-200 px-4 py-2 hover:bg-gray-100"
                             >
-                              {user.name}
-                            </label>
-                          </div>
-                        ))}
-                      </div>
+                              <input
+                                type="checkbox"
+                                id={`user-${user.id}`}
+                                className="form-checkbox h-5 w-5 text-blue-600 rounded focus:ring-blue-500"
+                                value={user.id}
+                                onChange={() => handleCheckboxChange(user.id)}
+                                checked={selectedUsers.includes(user.id)}
+                              />
+                              <label
+                                htmlFor={`user-${user.id}`}
+                                className="ml-3 text- text-gray-800 cursor-pointer truncate"
+                                style={{ fontSize: "0.775rem" }}
+                              >
+                                {user.name}
+                              </label>
+                            </div>
+                          ))}
+                        </div>
+                      </>
                     ) : (
                       <p className="text-sm text-gray-500">
                         No hay usuarios disponibles
@@ -313,7 +352,7 @@ const FormAltaNovedad = ({
                   <ReactQuill
                     theme="snow"
                     value={values.mensaje}
-                    onChange={(content) => setFieldValue('mensaje', content)}
+                    onChange={(content) => setFieldValue("mensaje", content)}
                     placeholder="Ingrese el mensaje"
                     className="mt-2 block w-full p-3 text-black bg-slate-100 rounded-xl focus:outline-orange-500"
                   />
@@ -325,7 +364,7 @@ const FormAltaNovedad = ({
                 <div className="sticky bottom-0 bg-white py-3 px-4">
                   <input
                     type="submit"
-                    value={novedad ? 'Actualizar' : 'Crear Novedad'}
+                    value={novedad ? "Actualizar" : "Crear Novedad"}
                     className="w-full bg-orange-500 py-2 px-5 rounded-xl text-white font-bold hover:bg-[#fc4b08] focus:outline-orange-100"
                   />
                 </div>
